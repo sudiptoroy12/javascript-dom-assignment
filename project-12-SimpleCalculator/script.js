213135f286da2f118a57193aec561f9608e108b1
sudiptoroy12 <sudipto1294@gmail.com>
2026-07-03 20:58:26 +0600

    function parameter added in simplecalculator

--- project-12-SimpleCalculator/script.js
+++ project-12-SimpleCalculator/script.js
@@ -1,31 +1,44 @@
+const number1 = document.querySelector("#number1");
+const number2 = document.querySelector("#number2");
+
+const btn = document.querySelector(".btn");
+const divadd = document.querySelector("#resultadd");
+const divsub = document.querySelector("#resultsub");
+const divmul = document.querySelector("#resultmul");
+const divdivide = document.querySelector("#resultdivide");
+
+   btn.addEventListener("click", () => {
+        const numberOne = Number(number1.value);
+        const numberTwo = Number(number2.value);
+
+        const resultadd = numberOne + numberTwo;
+        const resultsub = numberOne - numberTwo;
+        const resultmul = numberOne * numberTwo;
+        const resultdivide = numberOne / numberTwo;
+
+       divadd.innerText = `Add: ${resultadd}`;
+       divsub.innerText = `Subtract: ${resultsub}`;
+       divmul.innerText = `Multiply: ${resultmul}`;
+       divdivide.innerText =
+       numberTwo !== 0
+         ? `Divide: ${resultdivide}`
+        : `Divide: Can not divide by zero`;
+    });
 
 
-
-const number1 = document.querySelector("#number1")
-const number2 = document.querySelector("#number2")
-
-
-const btn = document.querySelector(".btn")
-const divadd = document.querySelector("#resultadd")
-const divsub = document.querySelector("#resultsub")
-const divmul = document.querySelector("#resultmul")
-const divdivide = document.querySelector("#resultdivide")
-
-
-
-function add(){
+//  function dia try kore dekhi
+/*  function add(){
     btn.addEventListener("click", ()=>{
 
         const numberOne = Number(number1.value)
         const numberTwo = Number(number2.value)
 
         const resultadd = numberOne + numberTwo
-        
+
         console.log(resultadd);
 
         divadd.innerText = `Add: ${resultadd}`
-        
-       
+
     })
 }
 function subtract(){
@@ -35,9 +48,9 @@ function subtract(){
         const numberTwo = Number(number2.value)
 
         const resultsub = numberOne - numberTwo
-        
+
         divsub.innerText = `Subtract: ${resultsub}`
-       
+
     })
 }
 function multiply(){
@@ -48,7 +61,7 @@ function multiply(){
 
         const resultmul = numberOne * numberTwo
         divmul.innerText = `Multiply: ${resultmul}`
-       
+
     })
 }
 function divide(){
@@ -59,75 +72,42 @@ function divide(){
 
         const resultdivide = numberOne / numberTwo
        divdivide.innerText = `Divide: ${resultdivide}`
-       
+
     })
 }
-
-
-
 
 add()
 subtract()
 multiply()
 divide()
 
+*/
 
-//  pore try korbo abar ektu different vabe korar jonno
-
-//         const num1 = number1.addEventListener("input", ()=> {
-//             const numberOne =  Number(number1.value)
-//             console.log(numberOne);
-            
-         
-           
-//         });
-
-//        const num2 = number2.addEventListener("input", ()=> {
-           
-//          const numberTwo = Number(number2.value)
-//          console.log(numberTwo);
-         
-           
-//         });
-
-//         console.log(num1);
-//         console.log(num2);
-        
-        
+//  function parameter dia korlam same jinis
 
 
- 
- 
-// function calculate(n1,n2){
-//     btn.addEventListener("click", ()=>{
+     /*   function calculate (numberOne,numberTwo){
 
-//         const resultadd = n1 + n2;
-//          const resultsub = n1 - n2
-//          const resultmul = n1 * n2
-//         const resultdivide = n1 / n2
-       
-//         console.log(resultadd);
+            const resultadd = numberOne+ numberTwo;
+            const resultsub = numberOne - numberTwo;
+            const resultmul = numberOne * numberTwo;
+            const resultdivide = numberOne / numberTwo;
 
-//         divadd.innerText = `Add: ${resultadd}`
-//         divsub.innerText = `Subtract: ${resultsub}`
-//          divmul.innerText = `Multiply: ${resultmul}`
-//          divdivide.innerText = `Divide: ${resultdivide}`
-//     })
-// }
-    
+            divadd.innerText = `Add: ${resultadd}`;
+            divsub.innerText = `Subtract: ${resultsub}`;
+            divmul.innerText = `Multiply: ${resultmul}`;
+            divdivide.innerText =
+                numberTwo !== 0
+                ? `Divide: ${resultdivide}`
+                : `Divide: Can not divide by zero`;
 
-// calculate(num1,num2)
-   
+        }
 
+        btn.addEventListener("click", () => {
+            const num1 = Number(number1.value);
+            const num2 = Number(number2.value);
+            calculate(num1,num2)
 
-
-   
-    
-// calculate()
-
-
-
-
-
-
-
+  
+        });
+        */
